7e741b68d73166d2bcf704d4187d06a97c86b277
dkrupkina <dkrupkina@TeamCity>
2026-02-10 16:52:35 +0000

    Cleanup old configuration files (TeamCity change in 'AWS EC2 Profile' project)

--- .teamcity_aws_ec2_settings/SubProject/Project.kt
+++
@@ -1,9 +0,0 @@
-package SubProject
-
-import jetbrains.buildServer.configs.kotlin.*
-import jetbrains.buildServer.configs.kotlin.Project
-
-object Project : Project({
-    id("SubProject")
-    name = "SubProject"
-})
